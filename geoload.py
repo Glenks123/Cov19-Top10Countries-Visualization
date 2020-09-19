3161bb18bc68c936a04fc0b50c563dd3d3801d28
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2020-09-19 15:29:10 +0300

    Update geoload.py

--- geoload.py
+++ geoload.py
@@ -33,7 +33,7 @@ ctx.verify_mode = ssl.CERT_NONE
 cur.execute('SELECT countriess FROM Country')
 count = 0
 
-#BECAUSE OF THIS I WASTED A WHOLE DAY! ADD CUR.FETCHALL()
+
 for country in cur.fetchall():
     count += 1
     if count>=10:
@@ -48,24 +48,23 @@ for country in cur.fetchall():
         parms['key'] = api_key
     #Ex {'query': 'United States', 'key': 42}
 
-    #If I open any url in a new tab, it will be all those json data
+
     url = serviceurl + urllib.parse.urlencode(parms)
     #print(url)
     print('Retrieving {}'.format(url))
 
     openurl = urlopen(url, context=ctx)
-    #Here data will be that json data which includes all the information about the place.
+
     data = openurl.read().decode()
     print('Retrieved {} characters {}'.format(len(data),  data[:20].replace('\n',' ')))
     try:
         js = json.loads(data)
     except:
-        #In case of unicode error's we'll just print out the json data
+
         print(data)
         continue  
 
-    #Here we are just checking if everything went well and the json file is well formed.
-    #In that json data, there is a key called 'status'. So, we are just confirming with that if everything went well
+
     if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS'):
         print('=== Failure To Retrieve ===')
         print(data)
